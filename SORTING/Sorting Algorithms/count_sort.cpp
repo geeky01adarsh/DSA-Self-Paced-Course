5633c0be0b43fb3a355a88ca3dd2a5bc2497ee87
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2022-05-12 18:29:31 +0530

    heap sort

--- SORTING/Sorting Algorithms/count_sort.cpp
+++ SORTING/Sorting Algorithms/count_sort.cpp
@@ -2,24 +2,32 @@
 #include <vector>
 using namespace std;
 
-void countSort(int arr[], int n, int k){
-    vector<int> count(k,0);
-    for(int i=0; i<n; i++) count[arr[i]]++;
-    for(int i=1; i<n; i++) count[i] +=count[i-1];
+void countSort(int arr[], int n, int k)
+{
+    vector<int> count(k+1, 0);
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+    for (int i = 1; i <= k; i++)
+        count[i] += count[i - 1];
     int output[n];
-    for(int i=n-1; i>=0; i--){
-        output[count[arr[i]]-1] =arr[i];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[count[arr[i]] - 1] = arr[i];
         count[arr[i]]--;
     }
-    for(int i=0; i<n; i++)
-    arr[i]=output[i];
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
 }
 
-int main(){
+int main()
+{
     int n, k;
-    cin>>n>>k;
+    cin >> n >> k;
     int arr[n];
-    for(int i=0; i<n; i++) cin>>arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     countSort(arr, n, k);
-    for(int j=0; j<n; j++) cout<<arr[j]<<" ";
+    // cout << "Hello";
+    for (int j = 0; j < n; j++)
+        cout << arr[j] << " ";
 }
